0c7b8efd94e95f645904163660a4aced46007a11
Clara <clarapachecodev@gmail.com>
2023-04-26 18:02:10 -0300

    Import ThemeProvider component from styled-components, import global css as GlobalStyles and import theme.js as Theme and use them to style App page component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from '../src/styles/global.js'
+import Theme from '../src/styles/theme.js'
+
 import { App } from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
