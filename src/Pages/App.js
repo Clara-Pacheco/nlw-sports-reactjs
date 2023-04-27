425f32ed9ad7a348016bf03b2cded37d41a5bd63
Clara <clarapachecodev@gmail.com>
2023-04-27 17:09:44 -0300

    Remove border from App Container css component and add animation for main element inside of Container css component

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -3,9 +3,24 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 580px;
   height: 854px;
-  border: 1px solid red;
+  /* border: 1px solid red; */
 
   margin: 32px auto 44px;
 
+  > main {
+    animation: fromBottom .7s backwards;
+
+    main:nth-child(1) {
+      animation-delay: 0;
+    }
+
+    main:nth-child(2) {
+        animation-delay: .3s;
+    }
+
+    main:nth-child(3) {
+        animation-delay: .6s;
+    }
+  }
 
 `;
