d247af71796bda44be36629d81da7a2f06d7b57f
Clara <clarapachecodev@gmail.com>
2023-04-27 15:48:49 -0300

    Import styled from styled-components and build Container css component and Image css component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,19 @@ export const Container = styled.div`
   width: 580px;
   height: 152px;
 
+`;
 
+export const Image = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
+  > img {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    border: 4px solid ${({ theme }) => theme.COLORS.SVG_INSTAGRAM};
+  }
 
 `;
+
