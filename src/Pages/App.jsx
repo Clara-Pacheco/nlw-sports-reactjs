1716ccc9896fd8fce81d41a2430481e0d1d1d714
Clara <clarapachecodev@gmail.com>
2023-04-27 16:28:07 -0300

    Import Section component and use it to build 3 sections inside App component passing title and subtitle as props

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,11 +1,23 @@
 import { Container } from "./App"
 import { Header } from '../components/Header/Header.jsx'
-
+import { Section } from '../components/Section/Section.jsx'
 
 export function App() {
   return (
     <Container>
       <Header />
+      <Section
+        title='Meus jogos'
+        subtitle='Os games que eu mais curto jogar!'
+      />
+        <Section
+          title='Canais e streamers'
+          subtitle='Lista de canais e transmissões que não perco!!'
+        />
+      <Section
+        title='Minhas redes'
+        subtitle='Se conecte comigo agora mesmo!'
+      />
     </Container>
   )
 }
