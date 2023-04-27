700d22b1b4963122dd9301bf3e79aeff24a898c8
Clara <clarapachecodev@gmail.com>
2023-04-27 14:43:16 -0300

    Create folder Pages containing App page component + App.js containing Container css component with stylizations for App page component

---
+++ src/Pages/App.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 580px;
+  height: 854px;
+  border: 1px solid red;
+
+  margin: 32px auto 44px;
+
+
+`;
---
+++ src/Pages/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./App"
+
+
+export function App() {
+  return (
+    <Container>
+      <h1> Teste </h1>
+    </Container>
+  )
+}
+
+
