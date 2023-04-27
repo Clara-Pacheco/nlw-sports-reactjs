e6b570a7d7bbf1b638286adb796815b758e3a426
Clara <clarapachecodev@gmail.com>
2023-04-27 15:47:23 -0300

    Import Container and Image css components from Header.js and build Header component adding an img element

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { Container, Image } from './Header.js'
+
+export function Header() {
+  return (
+    <Container>
+      <Image>
+        <img 
+            src='https://github.com/Clara-Pacheco.png'
+            alt='Imagem do usuário'
+        />
+      </Image>
+    </Container>
+  )
+}
+
