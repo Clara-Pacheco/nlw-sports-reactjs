ed90ada52a787d6fa2ab5dc7b932008f882b484e
Clara <clarapachecodev@gmail.com>
2023-04-28 10:43:44 -0300

    Remove main:nth-child() selectors

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -8,19 +8,10 @@ export const Container = styled.div`
   margin: 32px auto 44px;
 
   > main {
-    animation: fromBottom .7s backwards;
-
-    main:nth-child(1) {
-      animation-delay: 0;
-    }
-
-    main:nth-child(2) {
-        animation-delay: .3s;
-    }
-
-    main:nth-child(3) {
-        animation-delay: .6s;
-    }
+    animation: fromBottom .7s backwards;  
+    
   }
+
+   
 
 `;
