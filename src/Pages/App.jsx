97d8b806d07d0774edc5a4c406d753ecb8008837
Clara <clarapachecodev@gmail.com>
2023-04-28 10:41:21 -0300

    Import all the logo imgs to App component and structure game data using an array of objets

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -3,6 +3,38 @@ import { Header } from '../components/Header/Header.jsx'
 import { Section } from '../components/Section/Section.jsx'
 import { ListItem } from "../components/ListItem/ListItem.jsx"
 
+import InstagramLogo from '../assets/InstagramLogo.png'
+import TwitchLogo from '../assets/TwitchLogo.png'
+import TwitterLogo from '../assets/TwitterLogo.png'
+import YouTubeLogo from '../assets/YoutubeLogo.png'
+
+const gamesListData = [
+  {
+    url:'https://www.twitch.tv/directory/game/League%20of%20Legends',
+    imgUrl:'https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg',
+    textAlt:'Imagem do jogo League of Legends'
+  },
+
+  {
+    url:'https://www.twitch.tv/directory/game/VALORANT',
+    imgUrl:'https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg',
+    textAlt:'Imagem do jogo Valorant'
+  },
+
+  {
+    url:'https://www.twitch.tv/directory/game/Minecraft',
+    imgUrl:'https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg',
+    textAlt:'Imagem do jogo Minecraft'
+  },
+
+  {
+    url:'https://www.twitch.tv/directory/game/Teamfight%20Tactics',
+    imgUrl:'https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg',
+    textAlt:'Imagem do jogo TFT'
+  }
+ 
+]
+
 export function App() {
   return (
     <Container>
@@ -12,37 +44,44 @@ export function App() {
           title='Meus jogos'
           subtitle='Os games que eu mais curto jogar!'
         >
-          <ListItem
-            url='https://www.twitch.tv/directory/game/League%20of%20Legends'
-            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg'
-            textAlt='Imagem do jogo League of Legends'
-          />
-
-          <ListItem
-            url='https://www.twitch.tv/directory/game/VALORANT'
-            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg'
-            textAlt='Imagem do jogo Valorant'
-          />  
-
-          <ListItem
-            url='https://www.twitch.tv/directory/game/Minecraft'
-            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg'
-            textAlt='Imagem do jogo Minecraft'
-          />  
-
-          <ListItem
-            url='https://www.twitch.tv/directory/game/Teamfight%20Tactics'
-            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg'
-            textAlt='Imagem do jogo TFT'
-          />  
+          { 
           
-          </Section>
+            gamesListData.map((game) => {
+              return 
+           })
+
+          }
+
+        </Section>
        
         <Section
           title='Canais e streamers'
           subtitle='Lista de canais e transmissões que não perco!!'
         >
-          
+            <ListItem 
+              url='https://www.twitch.tv/maykbrito'
+              imgUrl='https://static-cdn.jtvnw.net/jtv_user_pictures/9ce11a2b-ec84-44b1-9c76-b8d29df5fef0-profile_image-150x150.png'
+              textAlt='Imagem de Mayk Brito'
+            />  
+
+            <ListItem
+              url='https://www.twitch.tv/jakeliny'
+              imgUrl='https://static-cdn.jtvnw.net/jtv_user_pictures/caaa9067-d25b-4efb-b7c7-93badfd041a9-profile_image-70x70.png'
+              textAlt='Imagem de Jakeliny'
+            /> 
+
+            <ListItem
+              url='https://www.twitch.tv/marcobrunodev'
+              imgUrl='https://static-cdn.jtvnw.net/jtv_user_pictures/9ee63f92-e4d2-4d3c-a235-1be4f642239a-profile_image-70x70.png'
+              textAlt='Imagem de Marco Bruno Dev'
+            />   
+
+            <ListItem
+              url='https://www.twitch.tv/rafaballerinii'
+              imgUrl='https://static-cdn.jtvnw.net/jtv_user_pictures/5d664b85-b516-4d64-b3e2-914e58a9d247-profile_image-70x70.png'
+              textAlt='Imagem de Rafa Ballerini'
+            />   
+            
 
         </Section>
 
@@ -50,6 +89,27 @@ export function App() {
           title='Minhas redes'
           subtitle='Se conecte comigo agora mesmo!'
         >
+            <ListItem
+              url='https://www.twitch.com'
+              imgUrl= {TwitchLogo}
+              textAlt='Logo Twitch'
+            />   
+              <ListItem
+              url='https://www.twitter.com'
+              imgUrl= {TwitterLogo}
+              textAlt='Logo Twitter'
+            />   
+            <ListItem
+              url='https://www.youtube.com'
+              imgUrl= {YouTubeLogo}
+              textAlt='Logo Youtube'
+            />   
+            
+            <ListItem
+              url='https://www.instagram.com'
+              imgUrl= {InstagramLogo}
+              textAlt='Logo Instagram'
+            />   
 
         </Section>
       </main>
