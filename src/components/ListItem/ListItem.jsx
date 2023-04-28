f663c8f0ee556fdc627dc7a961bdd95e57b5cdab
Clara <clarapachecodev@gmail.com>
2023-04-28 08:49:03 -0300

    Build ListItem component

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,11 +1,16 @@
 import { Container } from './ListItem.js'
 
-export function ListItem() {
+export function ListItem({ url, imgUrl, textAlt}) {
   return (
     <Container>
-
-
-      
+       <a 
+        target="_blank"
+        href={url}>
+        <img
+         src= {imgUrl}
+         alt= {textAlt}
+        />
+      </a>
     </Container>
   )
 }
