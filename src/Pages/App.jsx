dcc143040ed765ff990b9ca658743f70fff7a55f
Clara <clarapachecodev@gmail.com>
2023-04-28 09:21:37 -0300

    Pass ListItem component + url,imgUrl and textAlt as props as children of Section component

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,24 +1,57 @@
 import { Container } from "./App"
 import { Header } from '../components/Header/Header.jsx'
 import { Section } from '../components/Section/Section.jsx'
+import { ListItem } from "../components/ListItem/ListItem.jsx"
 
 export function App() {
   return (
     <Container>
       <Header />
       <main>
-        <Section
+        <Section 
           title='Meus jogos'
           subtitle='Os games que eu mais curto jogar!'
-        />
-          <Section
-            title='Canais e streamers'
-            subtitle='Lista de canais e transmissões que não perco!!'
+        >
+          <ListItem
+            url='https://www.twitch.tv/directory/game/League%20of%20Legends'
+            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/21779-188x250.jpg'
+            textAlt='Imagem do jogo League of Legends'
           />
+
+          <ListItem
+            url='https://www.twitch.tv/directory/game/VALORANT'
+            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/516575-188x250.jpg'
+            textAlt='Imagem do jogo Valorant'
+          />  
+
+          <ListItem
+            url='https://www.twitch.tv/directory/game/Minecraft'
+            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/27471_IGDB-188x250.jpg'
+            textAlt='Imagem do jogo Minecraft'
+          />  
+
+          <ListItem
+            url='https://www.twitch.tv/directory/game/Teamfight%20Tactics'
+            imgUrl='https://static-cdn.jtvnw.net/ttv-boxart/513143-188x250.jpg'
+            textAlt='Imagem do jogo TFT'
+          />  
+          
+          </Section>
+       
+        <Section
+          title='Canais e streamers'
+          subtitle='Lista de canais e transmissões que não perco!!'
+        >
+          
+
+        </Section>
+
         <Section
           title='Minhas redes'
           subtitle='Se conecte comigo agora mesmo!'
-        />
+        >
+
+        </Section>
       </main>
     </Container>
   )
